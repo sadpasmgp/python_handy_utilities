afc5f4798f888b2d840902888ded8b6fffd44986
Sadiq Pasha <86211117+sadpasmgp@users.noreply.github.com>
2022-09-07 21:10:06 +0530

    Fixed issue with improper resolution
    Defined to compress by more than 4 times in area of image

--- Photo_Compressor.py
+++ Photo_Compressor.py
@@ -7,7 +7,7 @@ from turtle import width
 import PIL
 from PIL import Image
 
-location = r"E:\SnapsLifetime"
+location = r"E:\Snaps Lifetime\202006 Simran Sadiq Nikah"
 os.chdir(location)
 filelist = os.listdir(location)
 
@@ -15,19 +15,26 @@ for file in filelist:
     #open the image
     with Image.open(file) as my_image:
 
-        #new widht and height in px
-        width = 3840
-        height = 2160
+        # get width and height
+        width,height = my_image.size
+  
+        # display width and height
+        print("The height of the image is: ", height)
+        print("The width of the image is: ", width)
 
         print("The original size of Image is: ", round(len(my_image.fp.read())/1024,2), "KB")
 
         #compressed the image
+        width = width//2
+        height = height//2
+        
         my_image = my_image.resize((width,height),PIL.Image.NEAREST)
         my_image = my_image.resize((width // 2, height // 2), resample=Image.ANTIALIAS)
 
         #save the image
-        my_image.save(f"E:\SnapsLifetime\compressed_images\{file}"+'compressedResized.jpg')
+        my_image.save(f"E:\Snaps Lifetime\compressed_images\{file}")
 
         #open the compressed image
-        #with Image.open('compressedResized.jpg') as compresed_image:
-            #print("The size of compressed image is: ", round(len(compresed_image.fp.read())/1024,2), "KB")
+        with Image.open(file) as compresed_image:
+            print("The size of compressed image is: ", round(len(compresed_image.fp.read())/1024,2), "KB")
+            print('\n')
